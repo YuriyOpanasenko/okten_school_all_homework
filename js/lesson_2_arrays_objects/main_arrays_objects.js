8ff216a83eed25fd9f4c0428a27aea8318c48e35
YuriyOpanasenko <uriyopanasenko@gmail.com>
2022-07-27 16:04:40 +0300

    JavaScript_lesson_2_arrays_objects Це потрібно нормально створити.І автори - це масив з об'єктами. Цикли поки не чіпаємо. Зробив.

--- js/lesson_2_arrays_objects/main_arrays_objects.js
+++ js/lesson_2_arrays_objects/main_arrays_objects.js
@@ -1,10 +1,20 @@
 // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 
-let serials = ["THE BOYS", "Arcane", "Star Trek", "Friends", "Rick and Morty", "Supernatural", "The Big Bang Theory", "Battlestar Galactica", "How I Met Your Mother", "Merlin"];
+let serials = ["THE BOYS", "Arcane", "Star Trek", "Friends", "Rick and Morty", "Supernatural", "The Big Bang Theory", 12345, true, false];
 console.log(serials);
-for (let i = 0; i < serials.length; i++) {
-    console.log(serials[i]);
-}
+// for (let i = 0; i < serials.length; i++) {
+//     console.log(serials[i]);
+// }
+console.log(serials[0]);
+console.log(serials[1]);
+console.log(serials[2]);
+console.log(serials[3]);
+console.log(serials[4]);
+console.log(serials[5]);
+console.log(serials[6]);
+console.log(serials[7]);
+console.log(serials[8]);
+console.log(serials[9]);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 
@@ -26,12 +36,28 @@ let book_3 = {
 
 // // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
 // //     Поле "автори" - масив. Кожен автор має поля name, age
-book_1['authors'] = ['J. K. Rowling', 56];
-book_2['authors'] = ['Miguel de Cervantes', 68];
-book_3['authors'] = ['William H. McRaven', 66];
-console.log(book_1);
-console.log(book_2);
-console.log(book_3);
+let book_4 = {
+    title: 'Harry Potter',
+    pageCount: 452,
+    genre: 'Fantasy',
+    authors: [{name: 'J. K. Rowling', age: 56}],
+}
+let book_5 = {
+    title: 'Don Kihot',
+    pageCount: 529,
+    genre: "Roman's",
+    authors: [{name: 'Miguel de Cervantes', age: 68}],
+}
+let book_6 = {
+    title: 'Make Your Bed',
+    pageCount: 159,
+    genre: 'Self development',
+    authors: [{name: 'William H. McRaven', age: 66}],
+}
+console.log(book_4);
+console.log(book_5);
+console.log(book_6);
+
 
 // - Створити масив з 10 об'єктами які описують сутніcть "користувач". Поля: name, username,password.' +
 // 'Вивести в консоль пароль кожного користувача
@@ -48,9 +74,20 @@ let user = [
     {name: 'Dir', username: 'Mozhlyvo_vin_i_ye_Askold', password: 'HY65h6h&tt'},
     {name: 'Sviatoslav', username: 'Ihorovych', password: 'H34trbgst'},
 ]
-for (let i = 0; i < user.length; i++) {
-     console.log(user[i].password);
-}
+// for (let i = 0; i < user.length; i++) {
+//      console.log(user[i].password);
+// }
+console.log([user[0].password]);
+console.log([user[1].password]);
+console.log([user[2].password]);
+console.log([user[3].password]);
+console.log([user[4].password]);
+console.log([user[5].password]);
+console.log([user[6].password]);
+console.log([user[7].password]);
+console.log([user[8].password]);
+console.log([user[9].password]);
+
 
 
 
